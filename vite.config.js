a7b0e7f82d4ddef717108887c37c281f05421320
zhaoyunfei <zyf120600@163.com>
2023-10-13 10:41:11 +0800

    3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'web',
+  publicPath: '.',
+  transpileDependencies: true,
+  // base: 'web',
   plugins: [vue()],
 });
